d5c2d53712007eed8d6de52cda959a8f6e628881
Abeharuki0821 <abe.h3@shinmaywa.co.jp>
2025-08-27 11:40:43 +0900

    mdn.jsの修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,14 +1,10 @@
-const date = new Date("2000-01-17T16:45:30");
+const today = Date.now();
+const date = new Date(today); 
+
 const [month, day, year] = [
   date.getMonth(),
   date.getDate(),
   date.getFullYear(),
 ];
 
-const [hour, minutes, seconds] = [
-  date.getHours(),
-  date.getMinutes(),
-  date.getSeconds(),
-];
-
-console.log(date.getFullYear()+"年"+(date.getMonth() + 1)+"月"+date.getDate()+"日"+date.getHours()+"時"+date.getMinutes()+"分"+date.getSeconds()+"秒");
+console.log(date.getFullYear()+"年"+(date.getMonth()+1)+"月"+date.getDate()+"日");
